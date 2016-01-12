e666479d24113df359f9439295ac1e67873ee999
anonymoussc <50c5ac69@opayq.com>
2016-01-13 01:53:38 +0700

    Modified src/ Cerberus

--- src/Cerberus/CerberusServiceProvider.php
+++ src/Cerberus/CerberusServiceProvider.php
@@ -1,10 +1,24 @@
-<?php namespace Cerberus;
+<?php
+/**
+ * CerberusServiceProvider.php
+ * Modified from https://github.com/rydurham/Sentinel
+ * by anonymous on 12/01/16 22:33.
+ */
 
+namespace Cerberus;
+
+use Artisan;
+use Hashids\Hashids;
+use ReflectionClass;
+use Cerberus\Commands\CerberusPublishCommand;
+use Cerberus\Repositories\Session\SentrySessionRepository;
+use Cerberus\Repositories\Group\SentryGroupRepository;
+use Cerberus\Repositories\User\SentryUserRepository;
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class CerberusServiceProvider extends ServiceProvider
 {
-
     /**
      * Indicates if loading of the provider is deferred.
      *
@@ -19,7 +33,40 @@ class CerberusServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Find path to the package
+        $cerberusFilename = with(new ReflectionClass('Cerberus\CerberusServiceProvider'))->getFileName();
+        $cerberusPath     = dirname($cerberusFilename);
 
+        // Register Artisan Commands
+        $this->registerArtisanCommands();
+
+        // Establish Fallback Config settings
+        $this->mergeConfigFrom($cerberusPath.'/../config/cerberus.php', 'cerberus');
+        $this->mergeConfigFrom($cerberusPath.'/../config/sentry.php', 'sentry');
+
+        // Establish Views Namespace
+        if (is_dir(base_path() . '/resources/views/cerberus')) {
+            // The package views have been published - use those views.
+            $this->loadViewsFrom(base_path() . '/resources/views/cerberus', 'Cerberus');
+        } else {
+            // The package views have not been published. Use the defaults.
+            $this->loadViewsFrom($cerberusPath . '/../views/bootstrap', 'Cerberus');
+        }
+
+        // Establish Translator Namespace
+        $this->loadTranslationsFrom($cerberusPath . '/../lang', 'Cerberus');
+
+        // Include custom validation rules
+        include $cerberusPath . '/../validators.php';
+
+        // Should we register the default routes?
+        if (config('cerberus.routes_enabled')) {
+            include $cerberusPath . '/../routes.php';
+        }
+
+        // Set up event listeners
+        $dispatcher = $this->app->make('events');
+        $dispatcher->subscribe('Cerberus\Listeners\UserEventListener');
     }
 
     /**
@@ -29,7 +76,42 @@ class CerberusServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Register the Sentry Service Provider
+        $this->app->register('Cerberus\SentryServiceProvider');
+
+       // Register the Vinkla/Hashids Service Provider
+        $this->app->register('Vinkla\Hashids\HashidsServiceProvider');
+
+        // Load the Sentry and Hashid Facade Aliases
+        $loader = AliasLoader::getInstance();
+        $loader->alias('Sentry', 'Cartalyst\Sentry\Facades\Laravel\Sentry');
+        $loader->alias('Hashids', 'Vinkla\Hashids\Facades\Hashids');
+
+
+        // Bind the User Repository
+        $this->app->bind('Cerberus\Repositories\User\CerberusUserRepositoryInterface', function ($app) {
+            return new SentryUserRepository(
+                $app['sentry'],
+                $app['config'],
+                $app['events']
+            );
+        });
+
+        // Bind the Group Repository
+        $this->app->bind('Cerberus\Repositories\Group\CerberusGroupRepositoryInterface', function ($app) {
+            return new SentryGroupRepository(
+                $app['sentry'],
+                $app['events']
+            );
+        });
+
+        // Bind the Session Manager
+        $this->app->bind('Cerberus\Repositories\Session\CerberusSessionRepositoryInterface', function ($app) {
+            return new SentrySessionRepository(
+                $app['sentry'],
+                $app['events']
+            );
+        });
     }
 
     /**
@@ -39,7 +121,20 @@ class CerberusServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return [];
+        return array('auth', 'sentry');
     }
 
+    /**
+     * Register the Artisan Commands
+     */
+    private function registerArtisanCommands()
+    {
+        $this->app['cerberus.publisher'] = $this->app->share(function ($app) {
+            return new CerberusPublishCommand(
+                $app->make('files')
+            );
+        });
+
+        $this->commands('cerberus.publisher');
+    }
 }
