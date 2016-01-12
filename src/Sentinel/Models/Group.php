2b4d588854f21985227d5f665c42ee70e1fb00a6
anonymoussc <50c5ac69@opayq.com>
2016-01-13 01:35:22 +0700

    New Models

---
+++ src/Sentinel/Models/Group.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Group.php
+ * Modified from https://github.com/rydurham/Sentinel
+ * by anonymous on 13/01/16 1:33.
+ */
+
+namespace Cerberus\Models;
+
+use Hashids;
+
+class Group extends \Cartalyst\Sentry\Groups\Eloquent\Group
+{
+    /**
+     * Use a mutator to derive the appropriate hash for this group
+     *
+     * @return mixed
+     */
+    public function getHashAttribute()
+    {
+        return Hashids::encode($this->attributes['id']);
+    }
+}
