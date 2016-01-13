c79c6edc02be59a95809277805e1747745c9bfab
anonymoussc <50c5ac69@opayq.com>
2016-01-14 00:21:54 +0700

    Modified hashids

--- src/config/hashids.php
+++ src/config/hashids.php
@@ -18,8 +18,7 @@ return [
     |
     */
 
-    'default' => 'main',
-
+    'default'     => 'main',
     /*
     |--------------------------------------------------------------------------
     | Hashids Connections
@@ -33,18 +32,17 @@ return [
 
     'connections' => [
 
-        'main' => [
-            'salt' => 'F28F86043514AEB26943F1A67F21B94251201F322199855F1DF1B982E3CF7C92',
-            'length' => 6,
-            'alphabet' => 'abcdefghijklmnopqrstuvwxyz1234567890'
+        'main'        => [
+            'salt'     => 'F28F86043514AEB26943F1A67F21B94251201F322199855F1DF1B982E3CF7C92',
+            'length'   => 6,
+            'alphabet' => 'abcdefghijklmnopqrstuvwxyz1234567890',
         ],
-
         'alternative' => [
-            'salt' => 'your-salt-string',
-            'length' => 'your-length-integer',
-            'alphabet' => 'your-alphabet-string'
+            'salt'     => 'your-salt-string',
+            'length'   => 'your-length-integer',
+            'alphabet' => 'your-alphabet-string',
         ],
 
-    ]
+    ],
 
 ];
