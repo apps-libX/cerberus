12dd30a0efd3cf9e73e4752fa5ba77b20a6f43cb
anonymoussc <50c5ac69@opayq.com>
2016-02-12 05:04:17 +0700

    Modified carbuncle

--- src/config/carbuncle.php
+++ src/config/carbuncle.php
@@ -5,25 +5,6 @@
  * by anonymous on 13/01/16 1:55.
  */
 
-/**
- * Part of the Carbuncle package.
- *
- * NOTICE OF LICENSE
- *
- * Licensed under the 3-clause BSD License.
- *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
- *
- * @package        Carbuncle
- * @version        2.0.0
- * @author         Einherjars LLC
- * @license        BSD License (3-clause)
- * @copyright  (c) 2011 - 2013, Einherjars LLC
- * @link           http://einherjars.com
- */
-
 return [
 
     /*
